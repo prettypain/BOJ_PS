fee3e90c83e20ed3f688710a8aa0955874857e8e
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-04-30 14:39:20 +0900

    Update 소트인사이드.py

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,14 +1,4 @@
-dic = {
-    "c=" : 0,
-    "c-" : 0,
-    "dz=" : 0,
-    "d-" : 0,
-    "lj" : 0,
-    "nj" : 0,
-    "s=" : 0,
-    "z=" : 0
-}
-List = list(map(int, list(input())))
-List.sort(reverse=True)
-for i in List:
+List = list(map(int, list(input())))#문자열에 list를 주면 1나씩 쪼개진다. 그 쪼개진걸 정수로 바꾼다.
+List.sort(reverse=True) #reverse 라는 옵션을 True로 설정함으로써 내림차순이 된다.
+for i in List:#정렬된 값들을 i에 넣어 반복한다.
     print(i,end="")
