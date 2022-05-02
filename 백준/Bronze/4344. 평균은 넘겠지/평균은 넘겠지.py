a2b68cc030e20ea0698e6dc52099b4b665cf817d
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-02 22:06:03 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -3,5 +3,7 @@ input = sys.stdin.readline
 for i in range(int(input())):
     tmp = list(map(int ,input().split()))
     avg = sum(tmp[1:])/tmp[0]
-    c = len(list((filter(lambda x : x>avg, tmp[1:]))))
+    c = 0
+    for i in tmp[1:]:
+        if i>avg: c+=1
     print(f"{c/tmp[0]*100:.3f}%")
