1fca098401f1457df0f67917d13083b185c42be9
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-04 00:05:08 +0900

    [Bronze I] Title: 이항 계수 1, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,3 +1,8 @@
+#https://ko.wikipedia.org/wiki/%EC%9D%B4%ED%95%AD_%EA%B3%84%EC%88%98
+#https://oper6210.tistory.com/76
+#https://help.acmicpc.net/judge/rte/RecursionError
+#이 문제는 재귀횟수를 늘린다고 해결되는게 아니라(메모리 초과...)
+#그러므로 그냥 for문으로 돌려서 값을 구하면 된다.(굉장히 물리적으로)
 def fac(n):
     res = 1
     for i in range(2,n+1):
