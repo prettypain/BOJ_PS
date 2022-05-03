c9965cd282ee10b0dbab0a46e64c21b01b46c655
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-04 07:31:00 +0900

    [Bronze III] Title: ACM 호텔, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,7 +1,5 @@
-n = int(input())
+n=int(input())
 for _ in range(n):
-    h,w,c = map(int, input().split())
-    if c%h==0:
-        print(f'{h}{(c//h):02d}')
-    else:
-        print(f'{c%h}{(c//h)+1:02d}')
+    h,w,c=map(int,input().split())
+    if c%h==0:print(f'{h}{(c//h):02d}')
+    else:print(f'{c%h}{(c//h)+1:02d}')
