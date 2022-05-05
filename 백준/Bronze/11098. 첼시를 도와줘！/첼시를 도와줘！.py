01e48fef2dd85fbdadb5ce8046cb1fbe792edd0d
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-05 22:15:59 +0900

    [Bronze I] Title: 첼시를 도와줘!, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11098. 첼시를 도와줘！/첼시를 도와줘！.py
+++ 백준/Bronze/11098. 첼시를 도와줘！/첼시를 도와줘！.py
@@ -1,8 +1,10 @@
+"""
+각 값과 이름을 받아 값을 키로 이름을 벨류로 정하고
+값들 중에서 가장 큰 값을 가져와 그에 매칭하는 벨류를 가져와 출력한다.
+"""
 import sys
-dic = {}
 input = sys.stdin.readline
-n = int(input())
-for i in range(n):
+for i in range(int(input())):
     dic = {}
     for _ in range(int(input())):
         price, name = input().split()
