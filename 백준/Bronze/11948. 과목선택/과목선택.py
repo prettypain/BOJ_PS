ed4bcad52a3eba3c1300b5bad4df2bcb9522c73d
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-06 13:22:12 +0900

    [Bronze IV] Title: 과목선택, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11948. 과목선택/과목선택.py
+++ 백준/Bronze/11948. 과목선택/과목선택.py
@@ -1,7 +1,1 @@
-a = int(input())#물
-b = int(input())#화
-c = int(input())#생
-d = int(input())#지
-e = int(input())#역
-f = int(input())#지뤼
-print(sum(sorted([a,b,c,d], reverse=True)[:3]) +max(e,f))
+print(sum(sorted([int(input()),int(input()),int(input()),int(input())])[-1:-4:-1])+max(int(input()),int(input())))
