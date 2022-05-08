2c1569f22a4d9c2e2889b992d4174d592888e8cf
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-08 22:24:03 +0900

    [Bronze I] Title: 2007년, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.py
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -1,12 +1,7 @@
 def month(mon):
-    if mon in [1, 3, 5, 7, 8, 10, 12]:
-        return 31
-    elif mon in [4, 6, 9, 11]:
-        return 30
+    if mon in [1, 3, 5, 7, 8, 10, 12]:return 31
+    elif mon in [4, 6, 9, 11]:return 30
     return 28
-dic = {1: 'MON', 2: 'TUE', 3: 'WED', 4: 'THU', 5: 'FRI', 6: 'SAT', 0: 'SUN'}
-
-m,d = map(int, input().split())
-for i in range(1,m):
-    d+= month(i)
-print(dic[d%7])
+dic={1:'MON',2:'TUE',3:'WED',4:'THU',5:'FRI',6:'SAT',0:'SUN'}
+m,d=map(int, input().split())
+print(dic[(sum(month(i)for i in range(1,m))+d)%7])
