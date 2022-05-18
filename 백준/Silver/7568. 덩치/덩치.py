674835a05530994713251b8fec09f251e80ac9da
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-18 21:41:45 +0900

    [Silver V] Title: 덩치, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,3 +1,10 @@
+"""
+내 랭크의 기준은 나보다 몸무게가 크면서 키도 큰 사람의 수 + 1이 내 랭크가 된다.
+이점을 이용하면
+나보다 큰(몸무계,키)사람의 수를 구해 그 수의 +1을 하면 그게 내 랭크가 된다.
+"""
+import sys
+input = sys.stdin.readline
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 rank = []
