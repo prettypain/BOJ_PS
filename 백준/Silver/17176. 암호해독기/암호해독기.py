2a74fbb54716e4fe98de5820ea8fcaf28ed9cdab
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-25 07:49:38 +0900

    [Silver V] Title: 암호해독기, Time: 540 ms, Memory: 166296 KB -BaekjoonHub

---
+++ 백준/Silver/17176. 암호해독기/암호해독기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+dic = {0:' ',1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L', 13: 'M',
+       14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X',
+       25: 'Y', 26: 'Z', 27: 'a', 28: 'b', 29: 'c', 30: 'd', 31: 'e', 32: 'f', 33: 'g', 34: 'h', 35: 'i', 36: 'j'
+       , 37: 'k', 38: 'l', 39: 'm', 40: 'n', 41: 'o', 42: 'p', 43: 'q', 44: 'r', 45: 's', 46: 't', 47: 'u', 48: 'v', 49: 'w', 50: 'x', 51: 'y', 52: 'z'}
+List = [" ",'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+n = int(input())
+arr = list(map(int, input().split()))
+t = input().strip()
+w = ""
+for i in arr[::-1]:
+    w+=dic[int(i)]
+pb = True
+for i in List:
+    if w.count(i) != t.count(i):
+        pb = False
+        break
+print("y" if pb else "n")
