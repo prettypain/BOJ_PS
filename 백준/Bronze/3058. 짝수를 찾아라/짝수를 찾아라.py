1e97733bf35491af0ea9f1ff79c0638b982f6b6f
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-29 21:41:40 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.py
+++ 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.py
@@ -1,5 +1,6 @@
+import sys
+input = sys.stdin.readline
 n=int(input())
 for i in range(n):
     List = list(filter(lambda x : x%2==0, map(int, input().split())))
     print(sum(List),min(List))
-    
