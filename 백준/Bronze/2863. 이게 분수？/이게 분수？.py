0116a26e27917f2317e6397cfbf15862e79ad339
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-30 06:35:01 +0900

    [Bronze III] Title: 이게 분수?, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2863. 이게 분수？/이게 분수？.py
+++ 백준/Bronze/2863. 이게 분수？/이게 분수？.py
@@ -1,4 +1,3 @@
-a,b = map(int, input().split())
-c,d = map(int, input().split())
-List = [a/c+b/d, c/d+a/b, d/b+c/a, b/a+d/c]
+a,b=map(int,input().split());c,d=map(int,input().split())
+List=[a/c+b/d,c/d+a/b,d/b+c/a,b/a+d/c]
 print(List.index(max(List)))
