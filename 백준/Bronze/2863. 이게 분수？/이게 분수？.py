b3242f3f4aeb4850ac275d30240bd3aeb8843b0d
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-30 06:33:35 +0900

    [Bronze III] Title: 이게 분수?, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2863. 이게 분수？/이게 분수？.py
+++ 백준/Bronze/2863. 이게 분수？/이게 분수？.py
@@ -1,6 +1,4 @@
 a,b = map(int, input().split())
 c,d = map(int, input().split())
 List = [a/c+b/d, c/d+a/b, d/b+c/a, b/a+d/c]
-dic = {}
-for i in range(4):dic[List[i]] = i
-print(dic[max(List)])
+print(List.index(max(List)))
