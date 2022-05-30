354d9cec566a3a4583d152a368518a71ee5b9942
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-31 07:03:33 +0900

    [Silver V] Title: 다리 놓기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -6,6 +6,8 @@ nCr = n!/(n-r)! * r!
 #https://www.acmicpc.net/board/view/24868
 중요한 포인트를 잡아줌
 '''
+import sys
+input = sys.stdin.readline
 def fac(target):#팩토리얼 함수(재귀보단 속도가 월등히 빠름)
     for i in range(target-1,0,-1):
         target*=i
