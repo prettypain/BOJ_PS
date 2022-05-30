2a4410858dd2b50b585d25733b58c16f60582fb5
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-31 07:04:19 +0900

    [Silver V] Title: 다리 놓기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -13,10 +13,6 @@ def fac(target):#팩토리얼 함수(재귀보단 속도가 월등히 빠름)
         target*=i
     return target
 
-
 for i in range(int(input())):
     r,n = map(int, input().split())
-    if r==n:
-        print(1)
-    else:
-        print(fac(n)//(fac(n-r) * fac(r)))
+    print(1 if r==n else fac(n)//(fac(n-r) * fac(r)))
