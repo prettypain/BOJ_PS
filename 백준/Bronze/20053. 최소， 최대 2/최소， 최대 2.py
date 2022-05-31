448255db53086497a985e0692ca32733803e1c91
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-05-31 22:03:39 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 984 ms, Memory: 183920 KB -BaekjoonHub

--- 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.py
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.py
@@ -1,13 +1,6 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 for _ in range(int(input())):
     input()
-    List = list(map(int, input().split()))
-    MAX = List[0]
-    MIN = List[0]
-    for i in List:
-        if i > MAX:
-            MAX = i
-        elif i < MIN:
-            MIN = i
-    print(MIN,MAX)
+    a=list(map(int,input().split()))
+    print(min(a),max(a))
