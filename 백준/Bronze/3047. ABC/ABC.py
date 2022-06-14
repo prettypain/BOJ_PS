7d5f6716310bd538d8a75cc897090004fa072017
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-06-15 06:22:40 +0900

    [Bronze II] Title: ABC, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/3047. ABC/ABC.py
+++ 백준/Bronze/3047. ABC/ABC.py
@@ -1,7 +1,2 @@
-List = sorted(list(map(int, input().split())))
-dic = {"A" : List[0],
-       "B" : List[1],
-       "C" : List[2]
-}
-for i in input():
-    print(dic[i],end=" ")
+List=sorted(map(int,input().split()))
+for i in input():print(List[ord(i)-65],end=" ")
