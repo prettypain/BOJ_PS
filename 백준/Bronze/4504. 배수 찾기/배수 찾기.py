53811efaf66ec37deee4e93b71bd177d87ae4b86
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-06-28 06:22:03 +0900

    [Bronze III] Title: 배수 찾기, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4504. 배수 찾기/배수 찾기.py
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.py
@@ -1,2 +1,2 @@
-n=int(input())
-while (t:=int(input()))!=0:print(t,'is NOT a multiple of'if t%n!=0 else'is a multiple of',str(n)+'.')
+n=input()
+while(t:=int(input()))!=0:print(t,'is NOT a multiple of'if t%int(n)!=0 else'is a multiple of',n+'.')
