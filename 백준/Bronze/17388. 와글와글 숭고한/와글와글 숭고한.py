0035f0a23910126544a9864552510d6797ee16e5
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-07-02 20:44:51 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.py
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.py
@@ -1,5 +1,1 @@
-import sys
-input = sys.stdin.readline
-name = ['Soongsil', 'Korea', 'Hanyang']
-arr = list(map(int, input().split()))
-print("OK" if sum(arr) >= 100 else name[arr.index(min(arr))])
+print("OK" if sum(a:=list(map(int, input().split())))>99 else['Soongsil','Korea','Hanyang'][a.index(min(a))])
