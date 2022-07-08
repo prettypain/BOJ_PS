4bf137471f42d4ff8d01a49dd78985c2e924fb83
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-07-08 10:05:08 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 160 ms, Memory: 43668 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,3 +1,10 @@
+'''
+이 문제의 핵심은 맵핑(딕셔너리)을 할 수 있는지 없는지 이다.
+배열(리스트)로 해결하려고 하면 시간초과가 발생하므로 맵핑을 해야한다.
+(또는 다른 빠른 자료구조를 사용하거나)
+'''
+import sys
+input = sys.stdin.readline
 n = int(input())
 dic = {}
 for i in range(n):
