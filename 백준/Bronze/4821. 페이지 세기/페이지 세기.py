28bf998e421c2b105343c4eee851caba40250a9a
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-07-08 10:58:32 +0900

    [Bronze I] Title: 페이지 세기, Time: 456 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4821. 페이지 세기/페이지 세기.py
+++ 백준/Bronze/4821. 페이지 세기/페이지 세기.py
@@ -1,6 +1,13 @@
+'''
+리스트를 이용해서 각 페이지마다 0을 주고
+인쇄를 했으면 1로 표시하므로써 추가적인 카운팅을 하지 않고
+sum함수를 거쳐 출력하면 답이 된다.
+'''
+import sys
+input = sys.stdin.readline
 while(s:=int(input()))!=0:
     arr = [0 for i in range(s+10)]
-    for i in input().split(','): 
+    for i in input().strip().split(','): 
         if not '-' in i:
             i = int(i)
             if i > s: continue #한 페이지인데 인쇄범위를 넘는 경우
