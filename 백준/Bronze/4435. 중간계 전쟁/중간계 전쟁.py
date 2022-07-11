f78a704a6210a4abc42f43f6cedee43b7e543f80
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-07-11 16:22:48 +0900

    [Bronze III] Title: 중간계 전쟁, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4435. 중간계 전쟁/중간계 전쟁.py
+++ 백준/Bronze/4435. 중간계 전쟁/중간계 전쟁.py
@@ -1,9 +1,8 @@
 import sys
 I=sys.stdin.readline
 for i in range(int(I())):
-    g = list(map(int,I().split()))
-    s = list(map(int,I().split()))
-    for idx,val in enumerate([1,2,3,3,4,10]): g[idx]*=val
-    for idx,val in enumerate([1,2,2,2,3,5,10]): s[idx]*=val
+    g,s = list(map(int,I().split())),list(map(int,I().split()))
+    for x,v in enumerate([1,2,3,3,4,10]):g[x]*=v
+    for x,v in enumerate([1,2,2,2,3,5,10]):s[x]*=v
     g,s=sum(g),sum(s)
     print(f'Battle {i+1}:',"No victor on this battle field"if g==s else("Good triumphs over Evil"if g>s else"Evil eradicates all trace of Good"))
