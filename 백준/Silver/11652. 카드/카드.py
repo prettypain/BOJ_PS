97010c72ac61c38fe356d2a947d0841535a06917
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-07-12 12:33:38 +0900

    [Silver IV] Title: 카드, Time: 156 ms, Memory: 46980 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+dic = {}
+for i in range(int(input())):
+    n = int(input())
+    if dic.get(n):
+        dic[n] +=1
+    else:
+        dic[n] = 1
+m = max(dic.values())
+for k,v in sorted(dic.items()):
+    if v==m:
+        print(k)
+        break
