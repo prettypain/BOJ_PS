5c6e35e90bea66d331614608e94327242f5371ae
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-07-13 19:58:25 +0900

    [Silver IV] Title: 차집합, Time: 660 ms, Memory: 145512 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -1,18 +1,4 @@
 a,b = map(int, input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-dic = {}
-c = 0
-lst = []
-for i in b:
-    dic[i] = 1
-
-for i in a:
-    if None==dic.get(i):
-        c+=1
-        lst.append(i)
-if c:
-    print(c)
-    print(" ".join(map(str, sorted(lst))))
-else:
-    print(0)
+t = set(map(int, input().split())) - set(map(int, input().split()))
+l=len(t)
+print(f'{l}\n{" ".join(map(str,sorted(list(t))))}' if l else l)
