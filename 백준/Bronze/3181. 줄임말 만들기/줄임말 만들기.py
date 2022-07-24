4a3d26704a7847adb2ed82c769571a210ac01bd3
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-07-24 19:52:08 +0900

    Update 줄임말 만들기.py
    i added my explanation
    설명을 추가했다.

--- 백준/Bronze/3181. 줄임말 만들기/줄임말 만들기.py
+++ 백준/Bronze/3181. 줄임말 만들기/줄임말 만들기.py
@@ -1,6 +1,12 @@
-lst = input().split()
+'''
+간단하다
+그냥 0번째가 아닌데 차단목록에 있는 것들만 처내고 나머지들의 0번째 문자를 대문자로
+바꾼다음 저장해 출력하면 끝~
+'''
+from sys import stdin
 t = ""
 arr = ['i', 'pa', 'te', 'ni', 'niti', 'a', 'ali', 'nego', 'no', 'ili']
+lst = stdin.readline().split()
 for i in range(len(lst)):
     if i!=0 and lst[i] in arr: continue
     t+= lst[i][0].upper()
