396bcddb835989c66625eb4a1e91d1a8c2bf5e1b
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-07-31 17:51:29 +0900

    [Bronze III] Title: 히스토그램, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/13752. 히스토그램/히스토그램.py
+++ 백준/Bronze/13752. 히스토그램/히스토그램.py
@@ -1,2 +1,7 @@
-I=input
-print("\n".join(map(lambda x:x*"=",[int(I())for i in range(int(I()))])))
+'''
+아주 간단한 문제이다.
+그냥 주어진 수만큼 출력한다.
+'''
+from sys import stdin
+I=stdin.readline
+print("\n".join(map(lambda x:x*"=",[int(I().strip())for i in range(int(I().strip()))])))
