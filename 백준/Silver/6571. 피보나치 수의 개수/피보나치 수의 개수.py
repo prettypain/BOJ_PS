b61bcbe43f34c477be07f2f398cc8e1e57b9f9b3
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-08-13 14:24:02 +0900

    [Silver III] Title: 피보나치 수의 개수, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/6571. 피보나치 수의 개수/피보나치 수의 개수.py
+++ 백준/Silver/6571. 피보나치 수의 개수/피보나치 수의 개수.py
@@ -1,5 +1,14 @@
+'''
+솔직히 이 문제 시간초과 날줄 알고 기대조차 하지 않았다.....ㅎ
+처음에 제출했을 때 틀린 부분을 설명해보자면
+a,b 범위가 1,2일때(최소의 경우)를 생각하지 않아서 오답떴다(이후 수정후 맞음)
+
+이 문제의 핵심은 bottom-up방식으로 진행해 효율적으로 문제를 해결하면 된다.
+그리고 매 증가마다 a<=f(i)<=b 인지 카운팅하고 출력하면 끝이다.
+'''
+from sys import stdin
 while 1:    
-    a,b = map(int, input().split())
+    a,b = map(int, stdin.readline().split())
     if a==b==0: break
     memo = [1,2]
     c = 1 if a<=1<=b else 0
