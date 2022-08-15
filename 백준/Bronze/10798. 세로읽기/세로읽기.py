95a6ad16abc36054b74ab3d0a21c41c93fd2e1b8
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-08-15 17:27:02 +0900

    [Bronze I] Title: 세로읽기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,4 +1,9 @@
-lst = [input() for i in range(5)]
+'''
+가로로 지나가는 와중에 해당 행의 길이가 탐색하는 열의 번호+1보다 작은 경우만
+패스해주고 나머진 전부 res에 저장하면 성공
+'''
+from sys import stdin
+lst = [stdin.readline().rstrip() for i in range(5)]
 ml = max(map(len, lst))
 res = ""
 for i in range(ml):
