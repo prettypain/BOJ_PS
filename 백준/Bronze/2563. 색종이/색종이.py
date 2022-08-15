6292b61df93f95ae2081d51e1f0860f7cec928cc
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-08-15 17:39:56 +0900

    [Bronze I] Title: 색종이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2563. 색종이/색종이.py
+++ 백준/Bronze/2563. 색종이/색종이.py
@@ -1,3 +1,10 @@
+'''
+그냥 x,y 입력 받아서  (x-1, y-1) ~ (x+9, y+9)까지에 해당하는 위치를 모두 1로 바꿔서
+최종적으로 합한 결과값을 출력하면 끝~
+같은 곳이 여러번 입력에 해당되도 어자피 1로 고정이라 중복 문제 해결
+'''
+from sys import stdin
+input = stdin.readline
 lst = [[0 for j in range(100)] for i in range(100)]
 n = int(input())
 for _ in range(n):
