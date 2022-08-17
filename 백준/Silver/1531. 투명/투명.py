a640bdb5ab0db3c52bd62af780877706432a8aa2
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-08-17 16:20:16 +0900

    [Silver V] Title: 투명, Time: 128 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1531. 투명/투명.py
+++ 백준/Silver/1531. 투명/투명.py
@@ -1,3 +1,13 @@
+'''
+생각보다 간단하다.
+그냥 x, y 범위가 주어졌을 때 그 범위만큼 2차원 배열에 +1 해주고
+최종적으로 m보다 높은 값만 카운팅 해준다(m 이하인 경우 보이기 떄문)
+원래 이렇게 반복문이 많으면 별로 좋지 못하다.
+(시간이 오래 걸리니까)
+하지만 이 문제는 테스트 케이스를 적게 주어지므로 이렇게 단순하게 풀어도 정답 처리.
+'''
+from sys import stdin
+input = stdin.readline
 lst = [[0 for j in range(100)] for i in range(100)]
 n, m = map(int, input().split())
 for _ in range(n):
