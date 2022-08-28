1d48765a630254f08553ea6eb43cffef457bb296
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-08-29 07:04:35 +0900

    [Silver V] Title: 부분 문자열, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/6550. 부분 문자열/부분 문자열.py
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.py
@@ -1,6 +1,14 @@
+'''
+s와 t가 입력으로 주어졌을 때
+t를 하나씩 반복하면서 s[j]와 같으면 c와 j를 각각1씩 증가해준다.
+최종적으로 c와 len(s)가 같다면(마지막 s[-1]이 맞으면 c도 최종적으로 len(s)와 같아짐)
+정답
+'''
+from sys import stdin
+input = stdin.readline
 while True:
     try:
-        s,t = input().split()
+        s,t = input().rstrip().split()
         c = 0
         j = 0
         for i in range(len(t)):
