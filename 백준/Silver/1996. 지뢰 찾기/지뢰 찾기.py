4d40ba6cc71a94bfa1de7b48fb38c08ef4c11904
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-08-31 07:15:35 +0900

    [Silver V] Title: 지뢰 찾기, Time: 3496 ms, Memory: 38816 KB -BaekjoonHub

--- 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.py
+++ 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.py
@@ -1,8 +1,18 @@
+'''
+문제를 해결할 수 있는 방법을 생각하는건 간단하지만 구현이 조금 귀찮다.
+x, y가 좌표 범위를 탈출하는지 지켜봐야 하고 반복문 더 쓰면 시간 초과가 발생할 것
+같아서 깡구현...
+
+모든 좌표에 가보고 각 좌표에 상, 하, 좌, 우, 4개의 대각선칸이 숫자면 그 값들을 모아서
+10이상이면 M으로 tmp에 추가하고 아니면 모은 값으로 추가하면 끝
+'''
+from sys import stdin
+input = stdin.readline
 def f(x,y):
     if x<0 or x>=n or y<0 or y>=n: return False
     return int(arr[x][y]) if arr[x][y].isdigit() else 0
 n = int(input())
-arr = [list(input()) for i in range(n)]
+arr = [list(input().rstrip()) for i in range(n)]
 res = []
 for x in range(n):
     tmp = ""
