b749fffc614ee1ff9a7acb7b497e15fbb5599f0d
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-09-13 06:50:45 +0900

    [Silver IV] Title: 문서 검색, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -1,3 +1,5 @@
-a=input()
-b=input()
-print(a.count(b))
+from sys import stdin
+input = stdin.readline
+#count는 중복 없는게 특징이라 보통 깡구현해서 문자열 탐색해왔는데
+#이 문제는 중복제거라... 혹시 했는데..헐....이게 문제냐 처음이다 이런문제
+print(input().rstrip().count(input().rstrip()))
