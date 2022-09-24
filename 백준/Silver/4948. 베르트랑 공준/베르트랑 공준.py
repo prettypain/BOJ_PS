38bbfca896dab42b94765cc562c6e735493b1f46
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-09-24 23:22:02 +0900

    [Silver III] Title: 베르트랑 공준, Time: 1856 ms, Memory: 41556 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -0,0 +1,10 @@
+def sieve(n):
+    lst = list(range(n+1))
+    lst[1] = 0
+    for i in range(2, int(n**0.5)+1):
+        if lst[i] == 0: continue
+        for j in range(i+i, n+1, i): lst[j] = 0
+    return list(filter(lambda x : x!=0, lst[t+1:]))
+
+while(t:=int(input()))!=0:
+    print(len(sieve(t*2)))
