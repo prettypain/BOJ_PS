ce13412d14a55d5018a3eaacd8a6529ca746a080
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-09-26 06:41:17 +0900

    [Silver IV] Title: Prime, Time: 88 ms, Memory: 34728 KB -BaekjoonHub

--- 백준/Silver/9842. Prime/Prime.py
+++ 백준/Silver/9842. Prime/Prime.py
@@ -1,3 +1,9 @@
+'''
+시간복잡도 최대한 줄일려고 n 입력 단위별로 구하는 범위를 다르게 해서 최적화 하려고
+했는데 일단 소수 10000개까지 구하게 짜고 돌렸는 완전 여유....
+
+tmi로 104729까지 구해야 소수 10000개 딱 나온다(직접 범위 구함 ㅋㅋㅋ)
+'''
 def sieve(n):
     lst = list(range(n+1))
     lst[1] = 0
@@ -5,6 +11,4 @@ def sieve(n):
         if lst[i] == 0: continue
         for j in range(i+i, n+1, i): lst[j] = 0
     return list(filter(lambda x : x!=0, lst))
-
-
-print(sieve(105000)[int(input())-1])
+print(sieve(104729)[int(input())-1])
