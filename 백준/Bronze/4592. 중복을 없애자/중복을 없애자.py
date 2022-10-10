3fcf90d76ce48c9bbce81937ea27149be1c260e7
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-10-10 15:17:03 +0900

    [Bronze II] Title: 중복을 없애자, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4592. 중복을 없애자/중복을 없애자.py
+++ 백준/Bronze/4592. 중복을 없애자/중복을 없애자.py
@@ -1,10 +1,8 @@
 while True:
     lst = list(map(int, input().split()))
     if lst[0] == 0: break
-    arr = []
-    lst.pop(0)
-    bf = 0
-    for i in lst:
+    arr, bf = [], 0
+    for i in lst[1:]:
         if i != bf: arr.append(i)
         bf = i
     print(*arr,"$")
