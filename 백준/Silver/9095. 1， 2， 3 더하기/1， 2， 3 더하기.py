4ee393737e378f4c1398b70c08cd8e199e9d4aef
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-10-21 07:18:14 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,15 +1,1 @@
-from itertools import product
-from sys import stdin
-input = stdin.readline
-
-lst = [1,2,3]
-t = int(input())
-for _ in range(t):
-    c = 0
-    n = int(input())
-    
-    for j in range(1, n+1):
-        for i in product(lst, repeat=j):
-            if sum(i) == n:
-                c += 1
-    print(c)
+print('\n'.join([['0','1','2','4','7','13','24','44','81','149','274'][int(input())]for _ in range(int(input()))]))
