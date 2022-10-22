35af3cc24afdf13b9b9a92cc19a9651d34336321
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-10-22 21:51:47 +0900

    [Bronze II] Title: Adding Reversed Numbers, Time: 980 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/3486. Adding Reversed Numbers/Adding Reversed Numbers.py
+++ 백준/Bronze/3486. Adding Reversed Numbers/Adding Reversed Numbers.py
@@ -1,5 +1,1 @@
-'''
-그냥 두개의 수를 각각 뒤집어서 더하고 뒤집어서 출력하면 끝~
-'''
-for i in range(int(input())):
-    print(int(str(sum(map(lambda x : int(x[::-1]), input().split())))[::-1]))
+for i in range(int(input())):print(int(str(sum(map(lambda x:int(x[::-1]),input().split())))[::-1]))
