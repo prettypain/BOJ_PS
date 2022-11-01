cc433d5db73959d49afab51b3517d3bfc3a10fd2
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2022-11-02 07:12:32 +0900

    [Silver IV] Title: 역원소 정렬, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/5648. 역원소 정렬/역원소 정렬.py
+++ 백준/Silver/5648. 역원소 정렬/역원소 정렬.py
@@ -1,3 +1,11 @@
+'''
+이 문제는 입력받는 방식이 귀찮을 뿐이지 문제 자체는 쉽다.
+그냥 입력받은 값을 뒤집어 정수형으로 바꿔 정렬후 출력하면 끝이다.
+'''
+from sys import stdin
+input = stdin.readline
+
+
 lst = input().split()
 n = int(lst.pop(0))
 while n!=len(lst): lst += input().split()
