6c91d8b940a5b03645443cc0927cdf565d398b59
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2023-01-11 19:54:50 +0900

    [Bronze III] Title: 신기한 수, Time: 292 ms, Memory: 37560 KB -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.py
+++ 백준/Bronze/17618. 신기한 수/신기한 수.py
@@ -1,6 +1,12 @@
 import sys
+def s(target):
+    s = 0
+    while target>0:
+        s += target%10
+        target//=10
+    return s
 n = int(sys.stdin.readline())+1
 lst = [0]*n
 for i in range(1,n):
-    if i%sum(map(int, str(i)))==0: lst[i] = 1
+    if i%s(i)==0: lst[i] = 1
 sys.stdout.write(f'{sum(lst[:n])}')
