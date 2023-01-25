6f70587968e5e79ea91be76e8221ea0e554da0cf
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2023-01-25 16:57:45 +0900

    [Bronze I] Title: 암호, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1855. 암호/암호.py
+++ 백준/Bronze/1855. 암호/암호.py
@@ -1,5 +1,4 @@
-n,t = int(input()), input()
-lst = [val[::-1] if idx%2 else val for idx, val in enumerate([t[i:i+n] for i in range(0, len(t), n)])]
+n,t=int(input()),input()
+lst=[val[::-1]if idx%2 else val for idx, val in enumerate([t[i:i+n]for i in range(0,len(t),n)])]
 for i in range(n):
-    for j in range(len(lst)):
-        print(lst[j][i],end="")
+    for j in range(len(lst)):print(lst[j][i],end="")
