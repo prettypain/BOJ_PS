05080ce8d2dc198d0cf685eb006ce37270ba54f5
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2023-03-02 20:07:20 +0900

    [Silver I] Title: 소수상근수, Time: 1104 ms, Memory: 71432 KB -BaekjoonHub

--- 백준/Silver/9421. 소수상근수/소수상근수.py
+++ 백준/Silver/9421. 소수상근수/소수상근수.py
@@ -1,6 +1,19 @@
+'''
+문제의 해결방법은 생각보다 간단하게 생각해 낼 수 있다.
+문제는 TLE이지만 다행이 시간은 넉넉했다.
+
+먼저 체를 통해서 n이하의 모든 소수를 구한다.
+이후 구한 소수들 중에서 상근수 즉
+각 자리수의 제곱의 합을 여러번 계산한 결과가 1이 되는 수를 모두 출력한다.
+
+즉 우리는 sieve와 check_sang 두 함수가 필요하다.
+그런데 3의 경우 1이 안나오고 무한히 제곱합을 계산한다.
+그런데 이런 경우 제곱합의 값이 루프처럼 다시 반복된다는 특징이 있다.
+즉 이전에 제곱합을 계산한 값이 다시 나온다면 그 수는 상근수가 아니다.
+이점 유의해서 구현하면 1000ms대 정답을 받을 수 있다.
+'''
+
 import sys
-#input = sys.stdin.readline
-#print = sys.stdou.write
 def sieve(n):
     arr = list(range(n+1))
     arr[1] = 0
@@ -17,7 +30,7 @@ def sang(n):
         else: tmp.append(n)
     return True
 
-n = int(input())
-lst = sieve(n if n<1000000 else 1000000)
+n = int(sys.stdin.readline())
+lst = sieve(n)
 for i in lst:
-    if sang(i): print(i)
+    if sang(i): sys.stdout.write(f'{i}\n')
