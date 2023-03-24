6d13541c3a2cf98646dadaf7369afcd6c8804712
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2023-03-24 19:25:11 +0900

    [Silver IV] Title: 오셀로 재배치, Time: 1096 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.py
+++ 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.py
@@ -1,12 +1,5 @@
 for _ in range(int(input())):
-    n = int(input())
-    df = input()
-    af = input()
-    change = [0, 0] #black, white
+    n,df,af,change = int(input()),input(),input(),[0, 0]#black, white
     for i in range(n):
         if df[i] != af[i]: change[0 if df[i]=="W" else 1] += 1
-
-    if not (change[0] and  change[1]): #[0, >0] or [>0, 0]
-        print(sum(change))
-    else:
-        print(max(change))
+    print(f'{max(change)}')
