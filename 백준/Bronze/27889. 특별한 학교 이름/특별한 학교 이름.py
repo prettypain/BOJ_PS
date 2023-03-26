3f31fde8f00976c20783a5b3687874af90805daf
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2023-03-26 21:15:56 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,9 @@
+t = input()
+if t=="NLCS":
+    print("North London Collegiate School")
+elif t=="BHA":
+    print("Branksome Hall Asia")
+elif t=="KIS":
+    print("Korea International School")
+else:
+    print("St. Johnsbury Academy")
