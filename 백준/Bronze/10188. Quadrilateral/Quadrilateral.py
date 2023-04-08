ef6e0871709f08d4dda0edd971822cf495e14f4c
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2023-04-08 16:07:56 +0900

    [Bronze IV] Title: Quadrilateral, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10188. Quadrilateral/Quadrilateral.py
+++ 백준/Bronze/10188. Quadrilateral/Quadrilateral.py
@@ -1,6 +1,5 @@
-n = int(input())
-for t in range(n):
+for _ in range(int(input())):
     x,y = map(int, input().split())
     for i in range(y):
         print("X"*x)
-    if t+1!=n:print()
+    print()
