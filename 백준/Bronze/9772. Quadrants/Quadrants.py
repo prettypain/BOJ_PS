5e15fb11d5f156791ec6b0eed7c2b6b89fde750a
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2023-06-18 09:52:11 +0900

    [Bronze IV] Title: Quadrants, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9772. Quadrants/Quadrants.py
@@ -0,0 +1,4 @@
+while True:
+    x,y = map(float, input().split())
+    print('Q1' if x>0 and y>0 else 'Q2' if x<0 and y>0 else 'Q3' if x<0 and y<0 else 'Q4' if x>0 and y<0 else "AXIS")
+    if x==0 and y==0: break
