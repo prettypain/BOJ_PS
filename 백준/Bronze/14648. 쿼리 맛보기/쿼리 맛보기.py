9494f3fdc92c1cea84822f356d722ca69137492c
wonwoo jung <66121515+prettypain@users.noreply.github.com>
2023-06-30 18:04:45 +0900

    [Bronze II] Title: 쿼리 맛보기, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14648. 쿼리 맛보기/쿼리 맛보기.py
+++ 백준/Bronze/14648. 쿼리 맛보기/쿼리 맛보기.py
@@ -1,9 +1,11 @@
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
 n,q = map(int, input().split())
 lst = list(map(int, input().split()))
 for _ in range(q):
     t = list(map(int, input().split()))
     if t[0] == 1:
-        print(sum(lst[t[1]-1:t[2]]))
+        print(f'{sum(lst[t[1]-1:t[2]])}\n')
         lst[t[1]-1],lst[t[2]-1] = lst[t[2]-1],lst[t[1]-1]
-    else:
-        print(sum(lst[t[1]-1:t[2]])-sum(lst[t[3]-1:t[4]]))
+    else:print(f'{sum(lst[t[1]-1:t[2]])-sum(lst[t[3]-1:t[4]])}\n')
